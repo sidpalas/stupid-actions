5331e1b49abac17dfbaf0b95e77c9545ca036f95
Sid Palas <sid.palas@gmail.com>
2020-05-27 15:42:33 -0700

    Use PAT in checkout action

--- recursive/check_counter_and_commit.sh
+++ recursive/check_counter_and_commit.sh
@@ -9,9 +9,6 @@ then
 else
     echo "Count okay... continuing";
     echo $(( $count + 1 )) > $COUNTER_FILE
-    git config --global user.email "sid.palas@gmail.com"
-    git config --global user.name "sid palas"
-    git remote set-url origin "https://sidpalas:$PERSONAL_GITHUB_TOKEN@github.com/sidpalas/stupid-actions.git"
     git add $COUNTER_FILE
     git commit -m "Incremented counter file"
     git push 
