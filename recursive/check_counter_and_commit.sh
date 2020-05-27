4569d876598086548fac62ad99c9ab8c007e1ef5
Sid Palas <sid.palas@gmail.com>
2020-05-27 15:44:16 -0700

    add email/user back

--- recursive/check_counter_and_commit.sh
+++ recursive/check_counter_and_commit.sh
@@ -9,6 +9,8 @@ then
 else
     echo "Count okay... continuing";
     echo $(( $count + 1 )) > $COUNTER_FILE
+    git config --global user.email "sid.palas@gmail.com"
+    git config --global user.name "sid palas"
     git add $COUNTER_FILE
     git commit -m "Incremented counter file"
     git push 
