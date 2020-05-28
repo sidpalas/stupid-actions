f78d55a3d6bc119039ddb85d8a4c5e0fce054c70
sid palas <sid.palas@gmail.com>
2020-05-28 16:54:30 -0700

    Incremented counter file

---
+++ exponential/check_counter_and_commit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+COUNTER_FILE=./exponential/counter.txt
+
+function increment_commit_push {
+    count=$(cat "$COUNTER_FILE") 
+    echo $(( $count + 1 )) > $COUNTER_FILE
+    echo incremented!
+    git add $COUNTER_FILE
+    git commit -m "Incremented counter file"
+    # --force required since we will encounter conflicts with parallel jobs
+    git push --force
+}
+
+count=$(cat "$COUNTER_FILE") 
+if (( $count > 5 ));
+then 
+    echo "Count too high... exiting";
+else
+    echo "Count okay... continuing";
+    git config --global user.email "sid.palas@gmail.com"
+    git config --global user.name "sid palas"
+    for ((i = 1; i <= $1; i++ ));
+    do
+    increment_commit_push
+    done
+fi; 
