4aad4df5806f21865c3da89e68ee07fcb723f009
sid palas <sid.palas@gmail.com>
2020-05-28 18:14:17 -0700

    add sleep and use random uuid tag

--- exponential/check_counter_and_tag.sh
+++ exponential/check_counter_and_tag.sh
@@ -3,11 +3,15 @@
 COUNTER_FILE=./exponential/counter.txt
 
 function increment_tag_push {
-    git tag -a "$GITHUB_SHA.$1" -m "GITHUB_SHA tag"
-    git push origin "$GITHUB_SHA.$1"
+    uuid=$(uuidgen)
+    git tag -a $uuid -m "GITHUB_SHA tag"
+    git push origin $uuid
 }
 
 count=$(cat "$COUNTER_FILE") 
+
+sleep 5
+
 if (( $count > 3 ));
 then 
     echo "Count too high... exiting";
